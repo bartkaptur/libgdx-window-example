f93f018c23ce4f503de757d2839b31a457455e66
= <sboychen@PCTE23778.cern.ch>
2017-05-04 10:51:37 +0200

    Added new window example (top right corner close button).

--- core/src/xyz/codingdaddy/WindowExampleMain.java
+++ core/src/xyz/codingdaddy/WindowExampleMain.java
@@ -1,33 +1,100 @@
 package xyz.codingdaddy;
 
+import xyz.codingdaddy.hud.window.WindowWithTopRightCornerCloseButton;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
-public class WindowExampleMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+/**
+ * Application rendering.
+ * 
+ * @author serhiy
+ */
+public class WindowExampleMain extends ApplicationAdapter implements InputProcessor {
+
+	private Stage stage;
+	private WindowWithTopRightCornerCloseButton windowWithTopRightCornerCloseButton;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		stage = new Stage();
+
+		windowWithTopRightCornerCloseButton = new WindowWithTopRightCornerCloseButton();
+		windowWithTopRightCornerCloseButton.setSize(400, 300);
+		windowWithTopRightCornerCloseButton.setModal(true);
+		windowWithTopRightCornerCloseButton.setVisible(true);
+		windowWithTopRightCornerCloseButton.setMovable(true);
+		windowWithTopRightCornerCloseButton.setPosition(Gdx.graphics.getWidth()/2 - windowWithTopRightCornerCloseButton.getWidth()/2, Gdx.graphics.getHeight()/2 - windowWithTopRightCornerCloseButton.getHeight()/2);
+		
+		stage.addActor(windowWithTopRightCornerCloseButton);
+		
+		InputMultiplexer multiplexer = new InputMultiplexer();
+		multiplexer.addProcessor(this);
+		multiplexer.addProcessor(stage);
+		Gdx.input.setInputProcessor(multiplexer);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		stage.draw();
+		stage.act();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		stage.dispose();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		if (keycode == Input.Keys.NUM_1) {
+			windowWithTopRightCornerCloseButton.setVisible(true);
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
---
+++ core/src/xyz/codingdaddy/hud/window/WindowWithTopRightCornerCloseButton.java
@@ -0,0 +1,56 @@
+package xyz.codingdaddy.hud.window;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+/**
+ * Window which features the close button in top right corner (button moved outside of the window bounds).
+ * 
+ * @author serhiy
+ */
+public class WindowWithTopRightCornerCloseButton extends Window {
+
+	private static final WindowStyle windowStyle;
+	private static final ImageButtonStyle closeButtonStyle;
+	static {
+		TextureAtlas textureAtlas = new TextureAtlas(Gdx.files.internal("windows.pack"));
+		
+		windowStyle = new WindowStyle(new BitmapFont(), Color.BLACK, new TextureRegionDrawable(textureAtlas.findRegion("window-1-background")));
+		closeButtonStyle = new ImageButtonStyle();
+		closeButtonStyle.imageUp = new TextureRegionDrawable(textureAtlas.findRegion("window-1-close-button"));
+	}
+	
+	/**
+	 * Default constructor.
+	 */
+	public WindowWithTopRightCornerCloseButton() {
+		super("", windowStyle);
+		
+		final Button closeButton = new ImageButton(closeButtonStyle);
+		closeButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				setVisible(false);
+			}
+		});
+		getTitleTable().add(closeButton).size(38, 38).padRight(10).padTop(0);
+	}
+	
+	@Override
+	public void draw(Batch batch, float parentAlpha) {
+		setClip(true);
+		super.draw(batch, parentAlpha);
+		setClip(false);
+	}
+
+}
